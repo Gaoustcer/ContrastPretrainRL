d3316d2dd6640a84f2a671ea7e3f131c194f1468
gaohaihan <2337284889@qq.com>
2023-02-19 17:06:46 +0800

    commit file

--- train/Simatraining.py
+++ train/Simatraining.py
@@ -9,6 +9,10 @@ from torch.utils.tensorboard import SummaryWriter
 import d4rl
 import gym
 import torch.nn.functional as F
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import os
+
 
 class Siamtrain(object):
     def __init__(self,path = "./log/Siam") -> None:
@@ -23,10 +27,33 @@ class Siamtrain(object):
         self.path = path
         self.writer = SummaryWriter(self.path)
         self.logid = 0
+        self.fig = plt.figure()
+        self.ax = plt.axes(projection = "3d")
+
+    def save(self):
+        # import os
+        modelpath = os.path.join(self.path,"model")
+        if os.path.exists(modelpath) == False:
+            os.mkdir(modelpath)
+        modelpath = os.path.join(modelpath,"siam")
+        torch.save(self.Siam,modelpath)
     
+    def validate(self,index):
+        picroot = os.path.join(self.path,'picture')
+
+        for states,actions,_,_ in tqdm(self.data):
+            _,projection,prediction = self.Siam.forward(states,actions)
+            self.ax.scatter(x = projection[:,0],y = projection[:,1],z = projection[:,2],c = 'r',s = 0.1)
+            self.ax.scatter(x = prediction[:,0],y = prediction[:,1],z = prediction[:,2],c = 'g',s = 0.1)
+        picroot = os.path.join(picroot,"{}.png".format(index))
+        self.fig.savefig(picroot)
+        plt.cla()
+            
     def train(self):
         for epoch in range(self.EPOCH):
+            self.validate(epoch)
             self.trainanepoch()
+        self.save()
     def trainanepoch(self):
         for states,actions,enhancestates,enhanceactions in tqdm(self.data):
             self.optimizer.zero_grad()
